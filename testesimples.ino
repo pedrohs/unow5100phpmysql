37974f29d79469278c19ca423554ccd4bd7c0765
andremroch <andremroch@gmail.com>
2016-01-30 11:21:43 -0200

    Create testesimples.ino

---
+++ testesimples.ino
@@ -0,0 +1,61 @@
+
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+char server[] = "http://verde.96.lt/arduino";   
+IPAddress ip(192, 168, 0, 177);
+
+EthernetClient client;
+
+void setup() {
+ 
+  Serial.begin(9600);
+  while (!Serial) {
+    ; 
+  }
+
+  // start the Ethernet connection:
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip);
+  }
+  // give the Ethernet shield a second to initialize:
+  delay(1000);
+  Serial.println("connecting...");
+
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("connected");
+    // Make a HTTP request:
+    client.println("GET /salvardados.php?sensor1=11.1&sensor2=22.1&sensor3=33.1");
+    client.println("Host: verde.96.lt/arduino");
+    client.println("Connection: close");
+    client.println();
+  } else {
+    // if you didn't get a connection to the server:
+    Serial.println("connection failed");
+  }
+}
+
+void loop() {
+ :
+  if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+
+    // do nothing forevermore:
+    while (true);
+  }
+}
